092b749bcd6d5fe1e9475f12e5b4e735158859ec
Jake Prem <jprem@hmbnet.com>
2017-08-25 15:59:29 -0400

    Moved SchedulePanel to its own component

---
+++ src/components/SchedulePanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="panel">
+    <p class="panel-heading">
+      My Schedule
+    </p>
+    <p class="panel-tabs">
+      Tue, Jan 6: 8:00 AM
+    </p>
+    <div class="panel-block is-active">
+      <ul>
+        <li>Other</li>
+      </ul>
+    </div>
+    <a class="panel-block">
+      <span class="panel-icon">
+        <i class="fa fa-tag"></i>
+      </span>
+      Hardware
+    </a>
+    <a class="panel-block">
+      <span class="panel-icon">
+        <i class="fa fa-tag"></i>
+      </span>
+      .NET
+    </a>
+  </div>
+</template>
--- src/views/Sessions.vue
+++ src/views/Sessions.vue
@@ -3,32 +3,7 @@
     <div class="container">
       <div class="columns">
         <div class="column is-3">
-          <div class="panel">
-            <p class="panel-heading">
-              My Schedule
-            </p>
-            <p class="panel-tabs">
-              Tue, Jan 6: 8:00 AM
-            </p>
-            <div class="panel-block is-active">
-              <ul>
-                <li>Other</li>
-              </ul>
-            </div>
-            <a class="panel-block">
-              <span class="panel-icon">
-                <i class="fa fa-tag"></i>
-              </span>
-              Hardware
-            </a>
-            <a class="panel-block">
-              <span class="panel-icon">
-                <i class="fa fa-tag"></i>
-              </span>
-              .NET
-            </a>
-          </div>
-
+          <schedule-panel></schedule-panel>
         </div>
         <div class="column is-6">
           <session-list :sessions="sessions"></session-list>
@@ -46,6 +21,7 @@ import sessions from '@/data/sessions'
 
 import SessionList from '@/components/SessionList'
 import TagPanel from '@/components/TagPanel'
+import SchedulePanel from '@/components/SchedulePanel'
 
 export default {
   data () {
@@ -55,7 +31,8 @@ export default {
   },
   components: {
     SessionList,
-    TagPanel
+    TagPanel,
+    SchedulePanel
   },
   computed: {
     tags () {
