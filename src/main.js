7caefe8a14676ba7f0cc790338c1d819afed88cf
Jake Prem <jprem@hmbnet.com>
2017-10-03 11:47:56 -0400

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-74836913-3'
+})
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
